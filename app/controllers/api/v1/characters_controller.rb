189772d925e814f7ac8d47a561e88df6f1830d61
Antonio <antoniobmirandagomes@gmail.com>
2021-03-22 16:28:07 -0300

    added rack cors to gemfile and adding index to characters

--- app/controllers/api/v1/characters_controller.rb
+++ app/controllers/api/v1/characters_controller.rb
@@ -1,4 +1,8 @@
 class Api::V1::CharactersController < ApplicationController
+    def index
+        characters = Character.all
+        render json: characters
+    end
     def show
         render json: Character.find(params[:id])
     end
